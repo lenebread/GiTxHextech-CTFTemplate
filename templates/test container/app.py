dae5d0d41dbc6c8a4791b833ba7b8fb90c98a82b
charlene <charlene.ng.23@gmail.com>
2024-04-08 02:24:31 +0800

    Update app.py

--- templates/test container/app.py
+++ templates/test container/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "LNC24{flag}"
+    return "HEX{flag}"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=1337)
